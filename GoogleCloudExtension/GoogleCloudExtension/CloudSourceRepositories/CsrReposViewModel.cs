f33d93f37e2c2bc07c0d27c51e8bfd9d9f577bfd
Deren Liao <derenl@google.com>
2017-06-22 23:09:38 -0700

    no message

--- GoogleCloudExtension/GoogleCloudExtension/CloudSourceRepositories/CsrReposViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudSourceRepositories/CsrReposViewModel.cs
@@ -18,10 +18,13 @@ using GoogleCloudExtension.DataSources;
 using GoogleCloudExtension.GitUtils;
 using GoogleCloudExtension.TeamExplorerExtension;
 using GoogleCloudExtension.Utils;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -299,8 +302,84 @@ namespace GoogleCloudExtension.CloudSourceRepositories
             var repoItem = CsrCreateWindow.PromptUser(projects);
             if (repoItem != null)
             {
-                SetCurrentRepo(repoItem.LocalPath);
+                if (repoItem != null)
+                {
+                    if (Repositories == null)
+                    {
+                        Repositories = new ObservableCollection<RepoItemViewModel>();
+                    }
+                    Repositories.Add(repoItem);
+                    SetCurrentRepo(repoItem.LocalPath);
+
+                    var msg = string.Format("The repository {0} has been created successfully.", repoItem.Name);
+                    msg += " " + string.Format("[Create a new project or solution]({0}) now.", repoItem.LocalPath);
+
+                    _teamExplorer.ShowMessage(msg,
+                        command: new ProtectedCommand(handler: () =>
+                        {
+                            SetDefaultProjectPath(repoItem.LocalPath);
+                            var serviceProvider = ShellUtils.GetGloblalServiceProvider();
+                            var solution = serviceProvider.GetService(typeof(SVsSolution)) as IVsSolution;
+                            solution?.CreateNewProjectViaDlg(null, null, 0);
+                            _teamExplorer.ShowHomeSection();
+                        }));
+                }
             }
+        }
+
+        const string NewProjectDialogKeyPath = @"Software\Microsoft\VisualStudio\14.0\NewProjectDialog";
+        const string MRUKeyPath = "MRUSettingsLocalProjectLocationEntries";
+        internal static string SetDefaultProjectPath(string path)
+        {
+            var old = String.Empty;
+            try
+            {
+                var newProjectKey = Registry.CurrentUser.OpenSubKey(NewProjectDialogKeyPath, true) ??
+                    Registry.CurrentUser.CreateSubKey(NewProjectDialogKeyPath);
+                Debug.Assert(newProjectKey != null, string.Format(CultureInfo.CurrentCulture,
+                    "Could not open or create registry key '{0}'", NewProjectDialogKeyPath));
+
+                using (newProjectKey)
+                {
+                    var mruKey = newProjectKey.OpenSubKey(MRUKeyPath, true) ?? Registry.CurrentUser.CreateSubKey(MRUKeyPath);
+                    Debug.Assert(mruKey != null, string.Format(CultureInfo.CurrentCulture,
+                        "Could not open or create registry key '{0}'", MRUKeyPath));
+
+                    using (mruKey)
+                    {
+                        // is this already the default path? bail
+                        old = (string)mruKey.GetValue("Value0", string.Empty,
+                            RegistryValueOptions.DoNotExpandEnvironmentNames);
+                        if (String.Equals(path.TrimEnd('\\'), old.TrimEnd('\\'),
+                            StringComparison.CurrentCultureIgnoreCase))
+                            return old;
+
+                        // grab the existing list of recent paths, throwing away the last one
+                        var numEntries = (int)mruKey.GetValue("MaximumEntries", 5);
+                        var entries = new List<string>(numEntries);
+                        for (int i = 0; i < numEntries - 1; i++)
+                        {
+                            var val = (string)mruKey.GetValue("Value" + i, String.Empty,
+                                RegistryValueOptions.DoNotExpandEnvironmentNames);
+                            if (!String.IsNullOrEmpty(val))
+                                entries.Add(val);
+                        }
+
+                        newProjectKey.SetValue("LastUsedNewProjectPath", path);
+                        mruKey.SetValue("Value0", path);
+                        // bump list of recent paths one entry down
+                        for (int i = 0; i < entries.Count; i++)
+                            mruKey.SetValue("Value" + (i + 1), entries[i]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //VsOutputLogger.WriteLine(string.Format(CultureInfo.CurrentCulture, "Error setting the create project path in the registry '{0}'", ex));
+                Debug.WriteLine(string.Format(CultureInfo.CurrentCulture,
+                    "Error setting the create project path in the registry '{0}'", ex));
+            }
+            return old;
         }
 
         /// <summary>
